6a7801dcfe5c21e8483ec815b0eae6b8c3536981
Jakob Bossek <info@jakobbossek.de>
2018-09-27 18:22:21 -0700

    update test file

--- test.R
+++ test.R
@@ -4,6 +4,13 @@ library(ecr)
 
 load_all(".")
 
+fitness.fun2 = function(ind, instance) {
+  tour.length = sapply(seq_len(ind$n.vehicles), function(v) {
+    computeTourLength(ind, instance, vehicle = v)
+  })
+  return(c(max(tour.length), ind$n.dynamic.inactive))
+}
+
 PATH.TO.INSTANCES = normalizePath("/Users/jboss/repositories/paper/orienteering_publications/PaperGecco2015/instances/publication/")
 
 SOLVER.PATH = c("eax" = file.path("/Users/jboss/repositories/paper/tsp_publications/TSPAS/solvers/EAX/main"))
@@ -14,12 +21,6 @@ aposteriori = read.table("inst/results/aposteriori.csv", header = TRUE)
 instance.path = file.path(PATH.TO.INSTANCES, " vrp_morphed_n100_mo25_dyn75_r1.csv")
 inst = " vrp_morphed_n100_mo25_dyn75_r1.csv"
 
-catf("Importing instance %s", instance.path)
-instance = salesperson::importFromFile(instance.path)
-#FIXME: handle in salesperson package
-instance$coords = rbind(instance$depot.coordinates, instance$coordinates)
-instance$dmat   = as.matrix(dist(instance$coords), method = "euclidean")
-
 # EMOA test environment
 debug = NA
 
@@ -29,39 +30,42 @@ instance = salesperson::importFromFile(instance.path)
 instance$coords = rbind(instance$depot.coordinates, instance$coordinates)
 instance$dmat   = as.matrix(dist(instance$coords), method = "euclidean")
 
+instance.apost = aposteriori
+
 st = proc.time()
-emoa.res = dynamicVRPEMOA(
-  fitness.fun, decision.fun = decideRank(1L, 0), instance = instance,
+emoa.res2 = dynamicVRPEMOA(
+  fitness.fun2, decision.fun = decideRank(1L, 0), instance = instance,
   mu = 10L, lambda = 5L,
-  local.search.method = "eax",
+  n.vehicles = 3L,
+  p.swap = 0.8,
+  local.search.method = NULL,
   local.search.gens = c(100),
-  init.keep = TRUE,
-  stop.conds = list(ecr::stopOnIters(100)),
-  #n.timeslots = 8L
+  init.keep = FALSE,
+  stop.conds = list(ecr::stopOnIters(10000)),
+  n.timeslots = 3L
   )
 time.passed = proc.time() - st
 
-fronts = lapply(emoa.res$era.results, function(x) x$front)
-fronts = lapply(fronts, function(x) {
-  x$f2 = x$f2shifted
-  return(x)
-})
-current.era = length(fronts)
+fronts = emoa.res2$pareto.front
 
-the.aposteriori = aposteriori[aposteriori$prob == gsub(" ", "", basename(inst)), , drop = FALSE]
-pl.fronts = plotEras(fronts, current.era, length(fronts) * 100L, a.posteriori.approx = the.aposteriori)
+# #stop("YAY!")
+# current.era = length(fronts)
 
-max.era = length(fronts)
+# the.aposteriori = aposteriori[aposteriori$prob == gsub(" ", "", basename(inst)), , drop = FALSE]
+# pl.fronts = plotEras(fronts, current.era, length(fronts) * 100L, a.posteriori.approx = the.aposteriori)
 
-dm.ind = emoa.res$era.results[[max.era]]$dm.choice.ind
-dm.ind.tour = getTourFromIndividual(dm.ind)
+max.era = max(fronts$era)
 
-pl.instance = autoplot(instance, path = c(1L, dm.ind.tour, 2L))
+dm.ind = emoa.res2$era.results[[max.era]]$dm.choice.ind
+dm.ind.tour = getToursFromIndividual(dm.ind, append.depots = TRUE)
+names(dm.ind.tour) = paste0("vehicle", 1:dm.ind$n.vehicles)
+
+pl.instance = autoplot(instance, path = dm.ind.tour)
 idx.man = which(instance$arrival.times == 0)
 pl.instance = pl.instance + geom_point(data = as.data.frame(instance$coordinates[idx.man, ]), colour = "red")
 
-pl.instance
-gridExtra::grid.arrange(pl.instance, pl.fronts, nrow = 1L)
+print(pl.instance)
+#gridExtra::grid.arrange(pl.instance, pl.fronts, nrow = 1L)
 
 
 stop("Done :)")
